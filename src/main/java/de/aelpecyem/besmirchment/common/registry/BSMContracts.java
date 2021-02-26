eb5a0130d1fe0c3869c482847b13a4172fb9419e
Aelpecyem <aelpecyem@gmail.com>
2021-02-26 19:48:00 +0100

    add conquest contract

---
+++ src/main/java/de/aelpecyem/besmirchment/common/registry/BSMContracts.java
@@ -0,0 +1,12 @@
+package de.aelpecyem.besmirchment.common.registry;
+
+import moriyashiine.bewitchment.api.registry.Contract;
+import moriyashiine.bewitchment.common.registry.BWRegistries;
+
+public class BSMContracts {
+    public static final Contract CONQUEST = new Contract(true);
+
+    public static void init(){
+        Util.register(BWRegistries.CONTRACTS,"conquest", CONQUEST);
+    }
+}
---
+++ src/main/java/de/aelpecyem/besmirchment/mixin/LivingEntityMixin.java
@@ -0,0 +1,36 @@
+package de.aelpecyem.besmirchment.mixin;
+
+import de.aelpecyem.besmirchment.common.registry.BSMContracts;
+import moriyashiine.bewitchment.api.interfaces.entity.ContractAccessor;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @ModifyVariable(method = "applyArmorToDamage", at = @At("HEAD"))
+    private float modifyDamage1(float amount, DamageSource source) {
+        if (source.isProjectile()) {
+            if (source.getAttacker() instanceof ContractAccessor) {
+                ContractAccessor accessor = (ContractAccessor) source.getAttacker();
+                if (accessor.hasContract(BSMContracts.CONQUEST)) {
+                    return amount * 2;
+                }
+            }
+            ContractAccessor accessor = (ContractAccessor)this;
+            if (accessor.hasNegativeEffects() && accessor.hasContract(BSMContracts.CONQUEST)){
+                return amount * 2;
+            }
+        }
+        return amount;
+    }
+}
