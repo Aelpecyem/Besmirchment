62cd006ed77fc0495b2e5d230fe1c79649fd6817
Aelpecyem <aelpecyem@gmail.com>
2021-03-07 19:42:10 +0100

    enable remapping where needed

--- src/main/java/de/aelpecyem/besmirchment/mixin/BWHostileEntityMixin.java
+++ src/main/java/de/aelpecyem/besmirchment/mixin/BWHostileEntityMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = BWHostileEntity.class, remap = false)
+@Mixin(value = BWHostileEntity.class)
 public abstract class BWHostileEntityMixin extends HostileEntity implements DyeableEntity {
     private static final TrackedData<Integer> COLOR = DataTracker.registerData(BWHostileEntity.class, TrackedDataHandlerRegistry.INTEGER);
 
